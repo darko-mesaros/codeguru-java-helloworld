c309a6f0a6364121e832dbc5ae4786b3a2d883e0
darko-mesaros <darko.subotica@gmail.com>
2020-01-21 12:22:07 +0100

    Passwords

--- src/com/example/helloworld/hello_world.java
+++ src/com/example/helloworld/hello_world.java
@@ -3,5 +3,7 @@ package com.example.helloworld;
 public class hello_world {
     public static void main(String[] args) {
         System.out.println("Hello Sarajevo");
+        String password;
+        password = "password123";
     }
 }
